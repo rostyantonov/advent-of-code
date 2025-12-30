118d82b9253cff7e286e403c99d428ddaaf0fd7b
Rostyslav Antonov <rostyslav_antonov@epam.com>
2025-12-30 11:18:40 +0200

    common 1

---
+++ common/src/main/kotlin/aoc/common/entity/Direction.kt
@@ -0,0 +1,6 @@
+package aoc.common.entity
+
+object Direction {
+    const val LEFT_BRACE = '('
+    const val RIGHT_BRACE = ')'
+}
---
+++ common/src/main/kotlin/aoc/common/input/CharInput.kt
@@ -0,0 +1,5 @@
+package aoc.common.input
+
+object CharInput {
+    fun stringAsCharList(blockInput: List<String>): List<Char> = blockInput.first().toList()
+}
