b6ebf060169f99251ef35bd8d2b36c2915846131
Rostyslav Antonov <rostyslav_antonov@epam.com>
2025-12-30 09:25:43 +0200

    Convert to multi-module project with 'main' module

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,8 @@ plugins {
     kotlin("jvm") version "2.3.0"
 }
 
-group = "advent.of.code"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-kotlin {
-    jvmToolchain(21)
-}
-
-tasks.test {
-    useJUnitPlatform()
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
