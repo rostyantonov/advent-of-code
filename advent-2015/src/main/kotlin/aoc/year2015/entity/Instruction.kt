9a511fb546672557862267fadd63b2edb89bc843
Rostyslav Antonov <rostyslav_antonov@epam.com>
2025-12-31 08:22:30 +0200

    Adding 2015 day06, and adding tests

---
+++ advent-2015/src/main/kotlin/aoc/year2015/entity/Instruction.kt
@@ -0,0 +1,19 @@
+package aoc.year2015.entity
+
+import aoc.common.entity.FieldConverter
+import aoc.common.entity.GenerateStructure
+import aoc.common.entity.Position
+import aoc.common.util.safeValue
+
+@GenerateStructure
+data class Instruction(
+    val cmd: String,
+    @FieldConverter(PositionConverter::class)
+    val start: Position,
+    @FieldConverter(PositionConverter::class)
+    val end: Position,
+) {
+    val command: Command = safeValue(cmd)
+    val xRange = start.xPos..end.xPos
+    val yRange = start.yPos..end.yPos
+}
