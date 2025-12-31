2ae4552f4ff8acf8d713b90940bc2ff477432b47
Rostyslav Antonov <rostyslav_antonov@epam.com>
2025-12-31 07:01:50 +0200

    common 3

--- common/src/main/kotlin/aoc/common/entity/Direction.kt
+++ common/src/main/kotlin/aoc/common/entity/Direction.kt
@@ -3,4 +3,9 @@ package aoc.common.entity
 object Direction {
     const val LEFT_BRACE = '('
     const val RIGHT_BRACE = ')'
+
+    const val UP_ARROW = '^'
+    const val DOWN_ARROW = 'v'
+    const val LEFT_ARROW = '<'
+    const val RIGHT_ARROW = '>'
 }
