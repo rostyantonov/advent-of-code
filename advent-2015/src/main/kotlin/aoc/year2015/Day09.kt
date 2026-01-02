5c76bf6fc0429ee05f111859d2e41847b67f14d4
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-01-02 15:39:39 +0200

    update 9

--- advent-2015/src/main/kotlin/aoc/year2015/Day09.kt
+++ advent-2015/src/main/kotlin/aoc/year2015/Day09.kt
@@ -39,17 +39,19 @@ class Day09 : AoCFileInput<List<Connection>, Int>() {
      * What is the shortest distance he can travel to achieve this?
      *
      * For example, given the following distances:
-     *   London to Dublin = 464
-     *   London to Belfast = 518
-     *   Dublin to Belfast = 141
+     *
+     *      London to Dublin = 464
+     *      London to Belfast = 518
+     *      Dublin to Belfast = 141
      *
      * The possible routes are therefore:
-     *   Dublin -> London -> Belfast = 982
-     *   London -> Dublin -> Belfast = 605
-     *   London -> Belfast -> Dublin = 659
-     *   Dublin -> Belfast -> London = 659
-     *   Belfast -> Dublin -> London = 605
-     *   Belfast -> London -> Dublin = 982
+     *
+     *      Dublin -> London -> Belfast = 982
+     *      London -> Dublin -> Belfast = 605
+     *      London -> Belfast -> Dublin = 659
+     *      Dublin -> Belfast -> London = 659
+     *      Belfast -> Dublin -> London = 605
+     *      Belfast -> London -> Dublin = 982
      *
      * The shortest of these is London -> Dublin -> Belfast = 605, and so the answer is 605 in this example.
      *
@@ -63,7 +65,9 @@ class Day09 : AoCFileInput<List<Connection>, Int>() {
      *
      * He can still start and end at any two (different) locations he wants,
      * and he still must visit each location exactly once.
-     * For example, given the distances above, the longest route would be 982 via (for example) Dublin -> London -> Belfast.
+     * For example, given the distances above, the longest route would be 982 via (for example)
+     *
+     *      Dublin -> London -> Belfast.
      *
      * What is the distance of the longest route?
      */
