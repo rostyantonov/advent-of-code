30be560aa3ade6c7a8253848edb5f3b14f9e50bc
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-01-02 15:40:06 +0200

    Adding 2015 day10, and adding tests

---
+++ advent-2015/src/test/kotlin/aoc/year2015/Day10Test.kt
@@ -0,0 +1,37 @@
+package aoc.year2015
+
+import aoc.common.AoCEmptyTest
+import aoc.common.IAoCSingleFunctionTest
+import org.junit.jupiter.params.provider.Arguments
+import java.util.stream.Stream
+import kotlin.test.BeforeTest
+
+// https://www.youtube.com/watch?v=ea7lJkEhytA
+class Day10Test :
+    AoCEmptyTest<Day10, Int>(),
+    IAoCSingleFunctionTest<Day10, Int, Int, String> {
+    override val partOneFunc: (Int) -> String
+        get() = currentDay::taskIterator
+
+    @BeforeTest
+    override fun setupCurrentDay() {
+        currentDay = Day10()
+    }
+
+    // 1 becomes 11 (1 copy of digit 1).
+    // 11 becomes 21 (2 copies of digit 1).
+    // 21 becomes 1211 (one 2 followed by one 1).
+    // 1211 becomes 111221 (one 1, one 2, and two 1s).
+    // 111221 becomes 312211 (three 1s, two 2s, and one 1).
+    override fun partOneFunctionInput(): Stream<Arguments> =
+        Stream.of(
+            Arguments.of("11", "1", 1),
+            Arguments.of("21", "11", 1),
+            Arguments.of("1211", "21", 1),
+            Arguments.of("111221", "1211", 1),
+            Arguments.of("312211", "111221", 1),
+            Arguments.of("312211", "1", 5),
+        )
+
+    override fun realResults(): Stream<Arguments> = Stream.of(Arguments.of(252_594, 3_579_328))
+}
