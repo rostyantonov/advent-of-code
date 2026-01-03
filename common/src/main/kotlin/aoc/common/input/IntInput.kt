f4b16bd4286caa980d4ec22ef887b201078b1896
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-01-03 08:54:11 +0200

    common 20

--- common/src/main/kotlin/aoc/common/input/IntInput.kt
+++ common/src/main/kotlin/aoc/common/input/IntInput.kt
@@ -1,5 +1,7 @@
 package aoc.common.input
 
 object IntInput {
+    fun getFirstInt(blockInput: List<String>): Int = blockInput.map { it.toInt() }.first()
+
     fun getIntList(blockInput: List<String>): List<Int> = blockInput.map { it.toInt() }
 }
