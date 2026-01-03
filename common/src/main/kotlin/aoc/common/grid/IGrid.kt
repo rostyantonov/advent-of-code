3ffca65f609a6f7290044e1c7f3c8bd16fd2e6a4
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-01-03 05:56:31 +0200

    common 18

---
+++ common/src/main/kotlin/aoc/common/grid/IGrid.kt
@@ -0,0 +1,31 @@
+package aoc.common.grid
+
+import aoc.common.entity.Position
+
+interface IGrid<Type> {
+    fun getValue(position: Position): Type?
+
+    fun getValue(
+        row: Int,
+        col: Int,
+    ): Type?
+
+    fun getNeighbours(
+        row: Int,
+        col: Int,
+    ) = getNeighbours(Position(row, col))
+
+    fun getNeighbours(position: Position): List<Type> =
+        listOfNotNull(
+            getValue(position.getUp()),
+            getValue(position.getUpRight()),
+            getValue(position.getRight()),
+            getValue(position.getDownRight()),
+            getValue(position.getDown()),
+            getValue(position.getDownLeft()),
+            getValue(position.getLeft()),
+            getValue(position.getUpLeft()),
+        )
+
+    fun clone(): IGrid<Type>
+}
