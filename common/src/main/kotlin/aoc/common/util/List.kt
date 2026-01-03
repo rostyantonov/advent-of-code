ec41f94d09ee0952458242d741945162164d605c
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-01-03 07:27:09 +0200

    common 19

--- common/src/main/kotlin/aoc/common/util/List.kt
+++ common/src/main/kotlin/aoc/common/util/List.kt
@@ -9,3 +9,13 @@ fun <T : IDataClass<T>> List<T>.cloneMutable(): MutableList<T> =
     mutableListOf<T>().also { list ->
         this.forEach { list.add(it.clone() as T) }
     }
+
+fun <Type> List<Type>.splitBy(predicate: (Type) -> Boolean): List<List<Type>> =
+    fold(mutableListOf(mutableListOf<Type>())) { acc, element ->
+        if (predicate(element)) {
+            acc.add(mutableListOf())
+        } else {
+            acc.last().add(element)
+        }
+        acc
+    }
