b4a7cc9de642bee64f90892ee6d2ccf2e899c2f4
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-04-03 21:52:57 +0300

    common 11

--- common/src/main/kotlin/aoc/common/entity/CharConstants.kt
+++ common/src/main/kotlin/aoc/common/entity/CharConstants.kt
@@ -1,7 +1,12 @@
 package aoc.common.entity
 
 object CharConstants {
+    const val CHAR_A_LOWERCASE = 'a'
+    const val CHAR_I_LOWERCASE = 'i'
+    const val CHAR_L_LOWERCASE = 'l'
+    const val CHAR_O_LOWERCASE = 'o'
     const val CHAR_V_LOWERCASE = 'v'
+    const val CHAR_Z_LOWERCASE = 'z'
 
     const val OPENING_ANGLE_BRACKET = '<'
     const val CLOSING_ANGLE_BRACKET = '>'
