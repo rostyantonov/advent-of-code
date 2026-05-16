4e445f0e380f0a27eee5db8f79b35141dd23d968
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-05-16 14:11:45 +0300

    Adding 2016 day22, and adding tests

---
+++ advent-2016/src/test/kotlin/aoc/year2016/Day22Test.kt
@@ -0,0 +1,31 @@
+package aoc.year2016
+
+import aoc.common.AoCEmptyTest
+import aoc.common.IAoCDoubleTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.provider.Arguments
+import java.util.stream.Stream
+
+class Day22Test :
+    AoCEmptyTest<Day22, Int>(),
+    IAoCDoubleTest<Day22, Int> {
+    @BeforeEach
+    override fun setupCurrentDay() {
+        currentDay = Day22()
+    }
+
+    override fun partOneInput(): Stream<Arguments> =
+        Stream.of(
+            Arguments.of(7, testRawInput),
+        )
+
+    override fun partTwoInput(): Stream<Arguments> =
+        Stream.of(
+            Arguments.of(7, testRawInput),
+        )
+
+    override fun realResults(): Stream<Arguments> =
+        Stream.of(
+            Arguments.of(903, 215),
+        )
+}
