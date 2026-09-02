de6a72910a15aebb16a1fb578daf6ed46c248c27
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-09-02 10:53:32 +0300

    common 2

--- common/src/main/kotlin/aoc/common/input/IntInput.kt
+++ common/src/main/kotlin/aoc/common/input/IntInput.kt
@@ -1,9 +1,16 @@
 package aoc.common.input
 
 object IntInput {
+    private val DIGITS_PATTERN = Regex("\\d+")
+
     fun getFirstInt(blockInput: List<String>): Int = blockInput.map { it.toInt() }.first()
 
     fun getIntList(blockInput: List<String>): List<Int> = blockInput.map { it.toInt() }
 
     fun getCharsAsIntList(blockInput: List<String>): List<Int> = blockInput.first().map { it.digitToInt() }
+
+    fun getIntOfIntList(blockInput: List<String>): List<List<Int>> =
+        blockInput.map { line ->
+            DIGITS_PATTERN.findAll(line).map { it.value.toInt() }.toList()
+        }
 }
