f73774a173c74849c91e1a1b9dd0d52a709a4456
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-09-02 10:53:32 +0300

    Common 2

--- common/src/main/kotlin/aoc/common/input/IntInput.kt
+++ common/src/main/kotlin/aoc/common/input/IntInput.kt
@@ -1,9 +1,16 @@
 package aoc.common.input
 
 object IntInput {
+    private val EMPTY_PATTERN = Regex("\\s+")
+
     fun getFirstInt(blockInput: List<String>): Int = blockInput.map { it.toInt() }.first()
 
     fun getIntList(blockInput: List<String>): List<Int> = blockInput.map { it.toInt() }
 
     fun getCharsAsIntList(blockInput: List<String>): List<Int> = blockInput.first().map { it.digitToInt() }
+
+    fun getIntOfIntList(blockInput: List<String>): List<List<Int>> =
+        blockInput.map { line ->
+            line.split(EMPTY_PATTERN).map { it.toInt() }
+        }
 }
