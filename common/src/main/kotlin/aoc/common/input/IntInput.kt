82d17ea5c7e883a6c4f4085feb856a1ac250f483
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-09-02 09:52:59 +0300

    common 1

--- common/src/main/kotlin/aoc/common/input/IntInput.kt
+++ common/src/main/kotlin/aoc/common/input/IntInput.kt
@@ -4,4 +4,6 @@ object IntInput {
     fun getFirstInt(blockInput: List<String>): Int = blockInput.map { it.toInt() }.first()
 
     fun getIntList(blockInput: List<String>): List<Int> = blockInput.map { it.toInt() }
+
+    fun getCharsAsIntList(blockInput: List<String>): List<Int> = blockInput.first().map { it.digitToInt() }
 }
