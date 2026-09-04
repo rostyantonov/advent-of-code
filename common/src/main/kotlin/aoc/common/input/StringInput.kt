27382fc5bc2dd7065d0f4883c880e5aea2f3036b
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-09-04 03:08:15 +0300

    Common 4

--- common/src/main/kotlin/aoc/common/input/StringInput.kt
+++ common/src/main/kotlin/aoc/common/input/StringInput.kt
@@ -1,7 +1,14 @@
 package aoc.common.input
 
 object StringInput {
+    private val EMPTY_PATTERN = Regex("\\s+")
+
     fun firstString(blockInput: List<String>): String = blockInput.first()
 
     fun asIs(blockInput: List<String>): List<String> = blockInput
+
+    fun getStringOfStringList(blockInput: List<String>): List<List<String>> =
+        blockInput.map { line ->
+            line.split(EMPTY_PATTERN)
+        }
 }
