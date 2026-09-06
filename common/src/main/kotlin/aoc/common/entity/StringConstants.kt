25ca5a07cb87c3d891e0a7c1fa8ec4d2fb6a8b8d
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-09-06 06:52:40 +0300

    common 8

---
+++ common/src/main/kotlin/aoc/common/entity/StringConstants.kt
@@ -0,0 +1,10 @@
+package aoc.common.entity
+
+object StringConstants {
+    const val STRING_GREATER = ">"
+    const val STRING_LESS = "<"
+    const val STRING_GREATER_OR_EQUAL = ">="
+    const val STRING_LESS_OR_EQUAL = "<="
+    const val STRING_EQUAL = "=="
+    const val STRING_NOT_EQUAL = "!="
+}
