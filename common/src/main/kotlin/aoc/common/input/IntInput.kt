cd134e5135f8143fb86245d48bf6ebd41bb364ef
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-09-08 07:23:45 +0300

    common 13

--- common/src/main/kotlin/aoc/common/input/IntInput.kt
+++ common/src/main/kotlin/aoc/common/input/IntInput.kt
@@ -8,6 +8,12 @@ object IntInput {
     fun getFirstLineIntList(blockInput: List<String>): List<Int> =
         DIGITS_PATTERN.findAll(blockInput.first()).map { it.value.toInt() }.toList()
 
+    fun getPairOfInt(blockInput: List<String>): List<Pair<Int, Int>> =
+        blockInput.map { line ->
+            val (first, second) = DIGITS_PATTERN.findAll(line).map { it.value.toInt() }.toList()
+            first to second
+        }
+
     fun getIntList(blockInput: List<String>): List<Int> = blockInput.map { it.toInt() }
 
     fun getCharsAsIntList(blockInput: List<String>): List<Int> = blockInput.first().map { it.digitToInt() }
